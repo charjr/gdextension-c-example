7b291ce5fb18e75ccf5d429028a46f17b49b0035
John <john@localhost.localdomain>
2025-10-21 19:52:44 +0100

    Add gdexample.c

---
+++ src/gdexample.c
@@ -0,0 +1,13 @@
+#include "gdexample.h"
+
+void gdexample_class_constructor(GDExample *self)
+{
+}
+
+void gdexample_class_destructor(GDExample *self)
+{
+}
+
+void gdexample_class_bind_methods()
+{
+}
