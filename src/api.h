1ba23cbf757b9d4862d710c437b5e345d42b2b6d
John <john@localhost.localdomain>
2025-10-21 19:54:47 +0100

    Fix api.h #11389
    api.h is missing `extern` keyword.
    
    This is required to avoid compilation errors.

--- src/api.h
+++ src/api.h
@@ -14,17 +14,17 @@ extern GDExtensionClassLibraryPtr class_library;
 
 // API methods.
 
-struct Constructors
+extern struct Constructors
 {
     GDExtensionInterfaceStringNameNewWithLatin1Chars string_name_new_with_latin1_chars;
 } constructors;
 
-struct Destructors
+extern struct Destructors
 {
     GDExtensionPtrDestructor string_name_destructor;
 } destructors;
 
-struct API
+extern struct API
 {
     GDExtensionInterfaceClassdbRegisterExtensionClass2 classdb_register_extension_class2;
 } api;
