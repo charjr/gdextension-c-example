5d8aeaf1cc6acdcd1ef0f4393bc5f188e066fc24
John <john@localhost.localdomain>
2025-10-21 20:00:56 +0100

    Fix typecasts in api.c
    This is not required, but can lead to compiler warnings otherwise.

--- src/api.c
+++ src/api.c
@@ -12,10 +12,10 @@ void load_api(GDExtensionInterfaceGetProcAddress p_get_proc_address)
     GDExtensionInterfaceVariantGetPtrDestructor variant_get_ptr_destructor = (GDExtensionInterfaceVariantGetPtrDestructor)p_get_proc_address("variant_get_ptr_destructor");
 
     // API.
-    api.classdb_register_extension_class2 = p_get_proc_address("classdb_register_extension_class2");
+    api.classdb_register_extension_class2 = (GDExtensionInterfaceClassdbRegisterExtensionClass2)p_get_proc_address("classdb_register_extension_class2");
 
     // Constructors.
-    constructors.string_name_new_with_latin1_chars = p_get_proc_address("string_name_new_with_latin1_chars");
+    constructors.string_name_new_with_latin1_chars = (GDExtensionInterfaceStringNameNewWithLatin1Chars)p_get_proc_address("string_name_new_with_latin1_chars");
 
     // Destructors.
     destructors.string_name_destructor = variant_get_ptr_destructor(GDEXTENSION_VARIANT_TYPE_STRING_NAME);
