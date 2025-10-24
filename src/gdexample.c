b32d9a4dbe37edcd26816dc86c9859420b8a16f9
John <john@localhost.localdomain>
2025-10-24 20:45:24 +0100

    Bind signal in gdexample.c

--- src/gdexample.c
+++ src/gdexample.c
@@ -63,6 +63,7 @@ void gdexample_class_bind_methods()
     bind_method_0_r("GDExample", "get_speed", gdexample_class_get_speed, GDEXTENSION_VARIANT_TYPE_FLOAT);
     bind_method_1("GDExample", "set_speed", gdexample_class_set_speed, "speed", GDEXTENSION_VARIANT_TYPE_FLOAT);
     bind_property("GDExample", "speed", GDEXTENSION_VARIANT_TYPE_FLOAT, "get_speed", "set_speed");
+    bind_signal_1("GDExample", "position_changed", "new_position", GDEXTENSION_VARIANT_TYPE_VECTOR2);
 }
 
 const GDExtensionInstanceBindingCallbacks gdexample_class_binding_callbacks = {
